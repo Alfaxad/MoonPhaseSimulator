b2f3411145d048186426a6630d7514ea634199f1
Alfaxad Eyembe <68440833+Alfaxad@users.noreply.github.com>
2025-06-04 12:31:38 +0900

    Add animation and three body simulator

--- MoonPhaseSimulator.py
+++ MoonPhaseSimulator.py
@@ -1,5 +1,8 @@
+"""Utility functions for visualising the lunar cycle."""
+
 from matplotlib import pyplot as plt
 from math import radians, sqrt, cos
+import numpy as np
 
 time_period = 29.5
 radius = 100
@@ -67,43 +70,63 @@ def ellipse_x_generator(y, r, angle):
     return abs(r * cos(radian_angle) * sqrt(1 - (y / r) ** 2))
 
 
-# Dealing with the moon(circle)
-y = list(range(-radius, (radius + 1)))
-circle_x_positive = []
-circle_x_negative = []
+def plot_moon_phase(days, ax=None):
+    """Plot the moon phase corresponding to ``days`` since new moon."""
+    if ax is None:
+        ax = plt.gca()
 
-for num in y:
-    circle_x_positive.append(circle_x_generator(num, radius))
-for num in circle_x_positive:
-    circle_x_negative.append(-num)
+    y = np.linspace(-radius, radius, 2 * radius + 1)
+    circle_x_positive = np.sqrt(radius ** 2 - y ** 2)
+    circle_x_negative = -circle_x_positive
 
-plt.plot(y, circle_x_positive, color="black")
-plt.plot(y, circle_x_negative, color="black")
-plt.axis("square")
+    ax.plot(y, circle_x_positive, color="black")
+    ax.plot(y, circle_x_negative, color="black")
+    ax.axis("square")
+    ax.set_xticks([])
+    ax.set_yticks([])
 
-# Getting user input for the days and finding initial values
-days = float(input("Enter the number of days since new moon: "))
-turn_angle = turn_angle_finder(days)
-terminus_side = terminus_side_finder(turn_angle)
-dark_side = dark_side_finder(turn_angle)
+    turn_angle = turn_angle_finder(days)
+    terminus_side = terminus_side_finder(turn_angle)
+    dark_side = dark_side_finder(turn_angle)
 
-# Dealing with the terminus(ellipse)
-ellipse_x = []
-if terminus_side == "right":
-    for num in y:
-        ellipse_x.append(ellipse_x_generator(num, radius, turn_angle))
-elif terminus_side == "left":
-    for num in y:
-        ellipse_x.append(-ellipse_x_generator(num, radius, turn_angle))
+    ellipse_x = radius * np.cos(np.radians(turn_angle)) * np.sqrt(1 - (y / radius) ** 2)
+    ellipse_x = np.abs(ellipse_x)
+    if terminus_side == "left":
+        ellipse_x = -ellipse_x
 
-plt.plot(ellipse_x, y, color="black")
+    ax.plot(ellipse_x, y, color="black")
 
-# Dealing with the moon shading(dark side)
-if dark_side == "right":
-    plt.fill_betweenx(y, circle_x_positive, ellipse_x, facecolor="grey")
-elif dark_side == "left":
-    plt.fill_betweenx(y, circle_x_negative, ellipse_x, facecolor="grey")
+    if dark_side == "right":
+        ax.fill_betweenx(y, circle_x_positive, ellipse_x, facecolor="grey")
+    else:
+        ax.fill_betweenx(y, circle_x_negative, ellipse_x, facecolor="grey")
 
-title = "Moon phase: " + phase_teller(turn_angle)
-plt.title(title)
-plt.show()
+    title = "Moon phase: " + phase_teller(turn_angle)
+    ax.set_title(title)
+
+
+def animate_moon_cycle(days=time_period, interval=300):
+    """Animate the lunar cycle for ``days`` days."""
+
+    fig, ax = plt.subplots()
+
+    from matplotlib.animation import FuncAnimation
+
+    def update(frame):
+        ax.cla()
+        plot_moon_phase(frame, ax)
+        return ax,
+
+    frames = np.linspace(0, days, int(days) + 1)
+    FuncAnimation(fig, update, frames=frames, interval=interval, repeat=True)
+    plt.show()
+
+
+def main():
+    days = float(input("Enter the number of days since new moon: "))
+    plot_moon_phase(days)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
